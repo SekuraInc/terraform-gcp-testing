39b6ca8a9e1acddc41553b0c99a8574ec5fb26e0
Hank <ffsclyh@gmail.com>
2019-06-24 15:05:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
-variable "machine_type" {
-    description = "GCP machine type"
-    default = "n1-standard-1"
+
+module "project-factory" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 2.0"
+
+  name                = "tmx-project-test-1"
+  random_project_id   = "true"
+  org_id              = "189920249297"
+  usage_bucket_name   = "tmx-project-test-1-bucket"
+  usage_bucket_prefix = "pf/test/1/integration"
+  billing_account     = "01E29D-DF668D-8C6E8B"
+  folder_id           = "747662050065"
+
 }
-
-variable "instance_name" {
-    description = "GCP instance name"
-    default = "demo"
-}
-
-variable "image" {
-    description = "GCP image"
-    default = "debian-cloud/debian-9"
-}
-
-
