6ac553ebd174c142f68b89b7355107998e62e1b0
Hank <ffsclyh@gmail.com>
2019-06-25 13:17:41 -0400

    test5

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+
+provider "google" {
+  credentials = "${var.gcp_credentials}"
+}
+
+resource "google_project" "new_project" {
+  count = "${length(var.projects)}"
+  name = "${element(var.projects, count.index)}"
+  project_id = "${element(var.projects, count.index)}"
+  org_id     = "${var.org_id}"
+  folder_id  = "${var.folder_id}"
+  billing_account = "${var.billing_account}"
+}
+
+
 variable "machine_type" {
     description = "GCP machine type"
     default = "n1-standard-1"
@@ -12,16 +27,3 @@ variable "image" {
     description = "GCP image"
     default = "debian-cloud/debian-9"
 }
-
-
-
-resource "google_project" "new_project" {
-  count = "${length(var.projects)}"
-  name = "${element(var.projects, count.index)}"
-  project_id = "${element(var.projects, count.index)}"
-  org_id     = "${var.org_id}"
-  folder_id  = "${var.folder_id}"
-  billing_account = "${var.billing_account}"
-}
-
-
