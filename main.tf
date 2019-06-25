9ab608c51f78bb049b7199bf2c2be69ac6437a96
Hank <ffsclyh@gmail.com>
2019-06-25 13:27:42 -0400

    test9

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "google_project" "new_project" {
   count = "${length(var.projects)}"
   name = "${element(var.projects, count.index)}"
   project_id = "${element(var.projects, count.index)}"
-  org_id     = "${var.org_id}"
   folder_id  = "${var.folder_id}"
   billing_account = "${var.billing_account}"
 }
