c61738aee4eb703a563bf618a88c6457698661b9
Hank <ffsclyh@gmail.com>
2019-06-25 13:08:10 -0400

    test1

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+variable "machine_type" {
+    description = "GCP machine type"
+    default = "n1-standard-1"
+}
+
+variable "instance_name" {
+    description = "GCP instance name"
+    default = "demo"
+}
+
+variable "image" {
+    description = "GCP image"
+    default = "debian-cloud/debian-9"
+}
+
 
 
 resource "google_project" "new_project" {
